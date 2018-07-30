320d31c3e0e890c4b55e45fa762f003ee5924f91
hawkin <mrpzhanghongjun@163.com>
2018-07-30 16:13:47 +0800

    remove debug message

--- env_setup.sh
+++ env_setup.sh
@@ -8,12 +8,10 @@ packages_list=additional_packages_list
 #echo "additional_packages_ubunturfs=$additional_packages_ubunturfs"
 echo installing additional packages: $additonal_package_type
 for type in ${additonal_package_type}; do
-	#type_name=$type
-	#echo "type_name=$type_name"
-	#echo "additional_packages_tool=$additional_packages_tool"
-	echo `eval echo  '$'"${type}"`
+
+	#echo `eval echo  '$'"${type}"`
 	package_type=`eval echo  '$'"${type}"`
-	echo "package_type=$package_type"
+	#echo "package_type=$package_type"
 	for pkg in $package_type; do
 		echo installing package  $pkg ...
 		sudo apt-get -y install $pkg || true
