2a7cbf2288e18cc016ca78014c91a917459c3232
hawkin zhang <mrpzhanghongjun@163.com>
2020-06-09 10:01:59 +0800

    rebuild gitpull manual script

--- gitpull_manual.sh
+++ gitpull_manual.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 script_path=$(dirname $(readlink -f "$0"))
-
-echo "update git code everyday $(date)" >> $script_path/gitpull.log
+echo $script_path
+echo "update github folder manual $(date)" > $script_path/gitpull.log
 
 packages_list=$script_path/gitfolder_list
 echo $packages_list
 . $packages_list
-#echo "additional_packages_ubunturfs=$additional_packages_ubunturfs"
-echo update gitcode everyday: $folder_type
-for type in ${folder_type}; do
-	#type_name=$type
-	#echo "type_name=$type_name"
-	#echo "additional_packages_tool=$additional_packages_tool"
-	echo `eval echo  '$'"${type}"`
-	folder_type=`eval echo  '$'"${type}"`
-	echo "folder_type=$folder_type"
-	for path in $folder_type; do
-		cd $path
-		echo update $path ...>> $script_path/gitpull.log
-		git pull &
+
+echo update gitcode everyday: $project_list
+for project_name in ${project_list}; do
+	echo "$project_name"
+
+	echo `eval echo  '$'"${project_name}"`
+	path_list=`eval echo  '$'"${project_name}"`
+
+	echo "path_list="$path_list
+
+	for project_path in $path_list;
+	do
+		echo "project_path="$project_path
+		for folder in `ls $project_path`
+		do
+			echo "update folder "$project_path/$folder
+			cd $project_path/$folder
+			echo update $project_path/$folder
+			echo update $project_path/$folder ...>> $script_path/gitpull.log
+			git pull &
+
+		done
 	done
+
+	
 done
-echo "update gitcode finished"
+echo "github folder update finished"
 
